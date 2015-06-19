51763487ae6ffe931e47b62204fcdb868f0c4386
David Windell <david@outeredgeuk.com>
2015-06-19 13:11:43 +0100

    tidy up run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
 
-while IFS=',' read -ra HOSTS; do
-    for i in "${HOSTS[@]}"; do
-        IFS=':' read -ra HOST <<< "$i"
+if [ -z "$ADDITIONAL_HOSTS" ]; then
+    while IFS=',' read -ra HOSTS; do
+        for i in "${HOSTS[@]}"; do
+            IFS=':' read -ra HOST <<< "$i"
 
-        cp /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/${HOST[0]}.conf
-        sed -i '/root/a server_name ${HOST[2]:-$HOST[0]};' /etc/nginx/conf.d/${HOST[0]}.conf
-        sed -i 's/ssl-cert.pem/ssl-cert-${HOST[0]}.pem/' /etc/nginx/conf.d/${HOST[0]}.conf
-        sed -i '/fastcgi_pass/i fastcgi_param MAGE_RUN_CODE ${HOST[1]:-$HOST[0]};' /etc/nginx/conf.d/${HOST[0]}.conf
-
-    done
-done <<< "$ADDITIONAL_HOSTS"
+            cp /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/${HOST[0]}.conf
+            sed -i '/root/a server_name ${HOST[1]};' /etc/nginx/conf.d/${HOST[0]}.conf
+            sed -i 's/ssl-cert.pem/ssl-cert-${HOST[0]}.pem/' /etc/nginx/conf.d/${HOST[0]}.conf
+            sed -i '/fastcgi_pass/i fastcgi_param MAGE_RUN_CODE ${$HOST[0]};' /etc/nginx/conf.d/${HOST[0]}.conf
+        done
+    done <<< "$ADDITIONAL_HOSTS"
+fi
 
 if [[ /var/www/app/etc/local.xml -ot /var/www/app/etc/local.$APPLICATION_ENV.xml ]]
 then
