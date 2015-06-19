a722bc5a3c3f8f0220144355604b4ff49982a080
David Windell <david@outeredgeuk.com>
2015-06-19 14:15:04 +0100

    Check if SSL certs exist for additional hosts

--- run.sh
+++ run.sh
@@ -7,8 +7,21 @@ if [ -z "$ADDITIONAL_HOSTS" ]; then
 
             cp /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/${HOST[0]}.conf
             sed -i '/root/a server_name ${HOST[1]};' /etc/nginx/conf.d/${HOST[0]}.conf
-            sed -i 's/ssl-cert.pem/ssl-cert-${HOST[0]}.pem/' /etc/nginx/conf.d/${HOST[0]}.conf
             sed -i '/fastcgi_pass/i fastcgi_param MAGE_RUN_CODE ${$HOST[0]};' /etc/nginx/conf.d/${HOST[0]}.conf
+
+            if [ -f "/etc/ssl/certs/ssl-cert-${HOST[0]}.pem" ]; then
+                sed -i 's/ssl-cert.pem/ssl-cert-${HOST[0]}.pem/' /etc/nginx/conf.d/${HOST[0]}.conf
+
+                if [ -f "/etc/ssl/private/ssl-cert-${HOST[0]}.key" ]; then
+                    sed -i 's/ssl-cert.key/ssl-cert-${HOST[0]}.key/' /etc/nginx/conf.d/${HOST[0]}.conf
+                fi
+
+                if [ -f "/etc/ssl/certs/ssl-chain-${HOST[0]}.pem" ]; then
+                    sed -i 's/ssl-chain.pem/ssl-chain-${HOST[0]}.pem/' /etc/nginx/conf.d/${HOST[0]}.conf
+                fi
+            else
+                sed -i '/listen 443/,/root/ {/root/!d}'
+            fi
         done
     done <<< "$ADDITIONAL_HOSTS"
 fi
