8bd8555dbba25bbf7b3e1bad9de567c4da761e0b
David Windell <david@outeredgeuk.com>
2018-11-15 13:18:08 +0000

    Upgrade to PHP 7.1.24, fix for multi-store

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM outeredge/edge-docker-php:7.1.17-alpine
+FROM outeredge/edge-docker-php:7.1.24-alpine
 
 ENV MAGENTO_VERSION=2.2.6
 
@@ -6,7 +6,7 @@ CMD ["/run.sh"]
 
 COPY . /
 
-RUN wget https://github.com/outeredge/edge-docker-magento/releases/download/v${MAGENTO_VERSION}/Magento-CE-${MAGENTO_VERSION}.tar.bz2 -O - | tar -jxf - -C /var/www --exclude='composer.lock' --exclude='*.md' && \
+RUN wget -nv https://github.com/outeredge/edge-docker-magento/releases/download/v${MAGENTO_VERSION}/Magento-CE-${MAGENTO_VERSION}.tar.bz2 -O - | tar -jxf - -C /var/www --exclude='composer.lock' --exclude='*.md' && \
     sed -i '/$relativePath = $request->getPathInfo();/a $relativePath = ltrim(ltrim($relativePath, "media"), "/");' /var/www/pub/get.php && \
     chmod +x /var/www/bin/magento && \
     cp /var/www/nginx.conf.sample /etc/nginx/magento_default.conf && \
--- run.sh
+++ run.sh
@@ -10,20 +10,6 @@ if [ ! -z "$ADDITIONAL_STORES" ]; then
             sed -i "/fastcgi_pass/i fastcgi_param MAGE_RUN_CODE ${HOST[0]};" /etc/nginx/conf.d/${HOST[0]}.conf
             sed -i 's/ reuseport//g' /etc/nginx/conf.d/${HOST[0]}.conf
             sed -i 's/ default_server//g' /etc/nginx/conf.d/${HOST[0]}.conf
-
-            if [ -f "/etc/ssl/certs/ssl-cert-${HOST[0]}.pem" ]; then
-                sed -i "s/ssl-cert.pem/ssl-cert-${HOST[0]}.pem/" /etc/nginx/conf.d/${HOST[0]}.conf
-
-                if [ -f "/etc/ssl/private/ssl-cert-${HOST[0]}.key" ]; then
-                    sed -i "s/ssl-cert.key/ssl-cert-${HOST[0]}.key/" /etc/nginx/conf.d/${HOST[0]}.conf
-                fi
-
-                if [ -f "/etc/ssl/certs/ssl-chain-${HOST[0]}.pem" ]; then
-                    sed -i "s/ssl-chain.pem/ssl-chain-${HOST[0]}.pem/" /etc/nginx/conf.d/${HOST[0]}.conf
-                fi
-            else
-                sed -i '/listen 443/,/root/ {/root/!d}' /etc/nginx/conf.d/${HOST[0]}.conf
-            fi
         done
     done <<< "$ADDITIONAL_STORES"
 fi
